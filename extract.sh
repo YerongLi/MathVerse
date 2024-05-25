9514092742a36b6687b6492116149356fb618740
yerong2 <yerong.li@outlook.com>
2024-05-25 05:36:11 +0000

    Upload the bash script for extract the anser

---
+++ extract.sh
@@ -0,0 +1,7 @@
+python evaluation/extract_answer_s1.py \
+	--model_output_file /home/yerong2/MathVerse/output_templates/output_testmini.json \
+	--save_file /home/yerong2/MathVerse/save.json \
+	--cache \
+	--trunk_response 30 \
+	--save_every 2 \
+	--api_key $OPENAIKEY
